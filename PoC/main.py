c698e49f0e5cc638d73adcad3aa4dc22491478ce
Tomasz <30781095+Scottigiusz@users.noreply.github.com>
2022-01-14 09:12:01 +0100

    Update main.py

--- PoC/main.py
+++ PoC/main.py
@@ -6,13 +6,60 @@ import json
 import plotly
 import plotly.express as px
 import plotly.graph_objects as go
+from cgitb import reset
+from logging import FileHandler
+from xml.etree.ElementTree import XML
+from pandas._libs.tslibs.timestamps import Timestamp
+import requests
 
 app = Flask(__name__)
 
-def getCrypto(crp, data_start, data_end):
+class RealTimeCurrencyConverter():
+    def __init__(self,url):
+        self.data= requests.get(url).json()
+        self.currencies = self.data['rates']
+
+    def convert(self, from_currency, to_currency, amount): 
+        initial_amount = amount 
+        #first convert it into USD if it is not in USD.
+        # because our base currency is USD
+        if from_currency != 'USD' : 
+          amount = amount / self.currencies[from_currency] 
+  
+        # limiting the precision to 4 decimal places 
+        amount = round(amount * self.currencies[to_currency], 4) 
+        return amount
+
+
+yf.pdr_override()
+pd.set_option("display.max_rows", None, "display.max_columns", None)
+
+def getCrypto(crp, data_start, data_end, type):
     crphistory = pdr.get_data_yahoo(crp, start=data_start, end=data_end)
     x = crphistory.to_dict()
-    return x["Open"]
+    return x[f"{type}"]
+
+def exchange(crp, data_start, data_end, currency, type):
+    url = 'https://api.exchangerate-api.com/v4/latest/USD'
+    converter = RealTimeCurrencyConverter(url)
+    x = getCrypto(crp, data_start, data_end, type)
+    dates = []
+    prices = []
+    exchanged_prices = []
+    for key, value in x.items():
+        dates.append(key)
+        prices.append(value)
+
+    for _ in prices:
+        exchanged_prices.append(converter.convert('USD', f'{currency}', _))
+
+    result = dict(zip(dates, exchanged_prices))
+
+    #filehandler = open("krypto.txt", "wt")
+    #data = str(result)
+    #filehandler.write(data)
+
+    return result
 
 def getGraph():
     df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv')
