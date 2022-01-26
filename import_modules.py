38f9ebfba02e709cb443136ac9f0b1b6ef5f2dd2
pruszyn130 <97448879+pruszyn130@users.noreply.github.com>
2022-01-26 10:23:59 +0100

    Update import_modules.py

--- import_modules.py
+++ import_modules.py
@@ -2,4 +2,4 @@ import sys
 import subprocess
 
 subprocess.check_call([sys.executable, '-m', 'pip', 'install',
-'plotly', 'flask', 'pandas', 'yfinance', 'CurrencyConverter', 'pandas-datareader', 'currency-symbols'])
+'plotly', 'flask', 'pandas', 'yfinance', 'pandas-datareader', 'currency-symbols'])
