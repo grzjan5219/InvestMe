09c2eb2fa139d3cebbc735296bcea8f007f1b9cf
Piotr Wittig <piotr.wittig@gmail.com>
2022-01-30 17:24:06 +0100

    Added table, period button placeholder, home page and trends

--- PoC/createFrontData.py
+++ PoC/createFrontData.py
@@ -1,9 +1,12 @@
-from percent import *
-from crc_conversion import *
 from currency_symbols import CurrencySymbols
+from currency_converter import CurrencyConverter
+import yfinance as yf
+from api import *
+import datetime
+from percent import *
+today = datetime.date.today()
 
 
-# create current name
 def currentNameFunc(crypto, currency):
     if crypto == 'ETH-USD':
         currentName = 'Ethereum'
@@ -14,20 +17,50 @@ def currentNameFunc(crypto, currency):
     elif crypto == 'DOGE-USD':
         currentName = 'Dogecoin'
     elif crypto == "USDT-USD":
-        currentName = 'Tether USD'
-    elif crypto == 'SOL1-USD':
-        currentName = 'Solana USD'
+        currentName = 'Tether'
+    elif crypto == 'AVAX-USD':
+        currentName = 'Avalanche'
+    elif crypto == 'SOL-USD':
+        currentName = 'Solana'
+    elif crypto == 'LINK-USD':
+        currentName = 'Chainlink'
     return currentName
 
 
-# creat dictionaries for cryptocurrencies list
+def get():
+    data = yf.download(tickers="BTC-USD ETH-USD DOGE-USD USDT-USD LINK-USD SOL-USD AVAX-USD BNB-USD", period='2d',
+                       interval='1d')['Close']
+    return data
+
+
+d = get()
+
+
+def ex(pricee, currency):
+    url = 'https://api.exchangerate-api.com/v4/latest/USD'
+    c = RealTimeCurrencyConverter(url)
+
+    return round(c.convert("USD", currency, float(pricee)), 2)
+
+
+def to_percentage(crypto):
+    crp_data = d[f'{crypto}'].to_dict()
+    crp_list = list(crp_data.values())
+    percentage = (crp_list[1] - crp_list[0]) / crp_list[0] * 100
+
+    return round(percentage, 2)
+
+
 def createDictCryptos(crypto, currency):
+    crp_data = d[f'{crypto}'].to_dict()
+    crp_list = list(crp_data.values())[1]
+    # print(crp_list)
     result = {}
     result["name"] = crypto
-    result["price"] = get(crypto, currency)
+    result["price"] = ex(crp_list, currency)
     result["img"] = "{{url_for('static', filename='img/{crypto}')}"
-    result["percentage"] = to_percentage(crypto)
     result["symbol"] = CurrencySymbols.get_symbol(currency)
+    result["percentage"] = to_percentage(crypto)
 
     return result
 
@@ -35,9 +68,28 @@ def createDictCryptos(crypto, currency):
 def addToList(currency):
     cryptos = []
     cryptos.extend((createDictCryptos('BTC-USD', currency),
-                    createDictCryptos('ETH-USD', currency),
-                    createDictCryptos('BNB-USD', currency),
-                    createDictCryptos('DOGE-USD', currency),
+                   createDictCryptos('ETH-USD', currency),
+                   createDictCryptos('BNB-USD', currency),
+                    createDictCryptos('SOL-USD', currency),
+                    createDictCryptos('AVAX-USD', currency),
+                    createDictCryptos('LINK-USD', currency),
                     createDictCryptos('USDT-USD', currency),
-                    createDictCryptos('SOL1-USD', currency)))
+                   createDictCryptos('DOGE-USD', currency)
+                    ))
     return cryptos
+
+
+def predict(crypto, currency):
+    timeline = exchange(crypto, '2022-01-01', '2022-01-27', currency)
+    tydzien = []
+    for i in range(3):
+        tydzien.append(timeline["Open"][-i])
+    tydzien_2 = []
+    for i in range(3, 6):
+        tydzien_2.append(timeline["Open"][-i])
+    if sum(tydzien)/len(tydzien) > sum(tydzien_2)/len(tydzien_2):
+        return("Up trend")
+    elif sum(tydzien)/len(tydzien) == sum(tydzien_2)/len(tydzien_2):
+        return("Flat trend")
+    else:
+        return("Down trend")
--- PoC/main.py
+++ PoC/main.py
@@ -9,32 +9,42 @@ from crc_conversion import *
 from createFrontData import *
 app = Flask(__name__)
 
+cryptos = addToList("USD")
+
 
 @ app.route('/')
 def home():
-    return render_template("home.html")
+
+    return render_template("home.html", cryptos=cryptos)
 
 
-@ app.route('/<currency>/<crypto>/<time>')
-def home1(currency, crypto, time):
+@ app.route('/<currency>/<crypto>/<time>/<interval>/')
+def home1(currency, crypto, time, interval):
     # get graph
-    fig = getGraph(currency, crypto, time)
+    fig = getGraph(currency, crypto, time, interval)
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
     # list with data abaout current cryptocurrency to display
+    c = d[f'{crypto}'].to_dict()
+    a = list(c.values())[1]
     currentData = [
         {
             'currentSymbol': CurrencySymbols.get_symbol(currency),
             'currentName': currentNameFunc(crypto, currency),
-            'currentPrice':  round(get(crypto, currency), 1),
+            'currentPrice':  ex(a, currency),
             'currentProcentage': to_percentage(crypto)
         }
     ]
 
+    prediction = predict(crypto, currency)
+
     # list with cryptocurrencies data to display
     cryptos = addToList(currency)
 
-    return render_template("crypto.html", currentData=currentData, cryptos=cryptos, graphJSON=graphJSON, crypto=crypto, time=time, currency=currency)
+    x = getCrypto(crypto, '2022-01-01', today)
+    result = x.to_html()
+
+    return render_template("crypto.html", currentData=currentData, cryptos=cryptos, graphJSON=graphJSON, crypto=crypto, time=time, currency=currency, result=result, prediction=prediction, interval=interval)
 
 
 if __name__ == '__main__':
