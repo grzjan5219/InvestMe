fcd752660dab51ebec437324cf86b3f7f95eb127
Tomasz <30781095+Scottigiusz@users.noreply.github.com>
2022-01-31 21:53:54 +0100

    update api.py
    from now on it doesn't download for example all 365 days if we set a year time but 30day interval

--- PoC/api.py
+++ PoC/api.py
@@ -4,6 +4,8 @@ import requests
 import datetime
 from pandas_datareader import data as pdr
 
+global trend
+
 
 class RealTimeCurrencyConverter():
     def __init__(self, url):
@@ -26,57 +28,29 @@ yf.pdr_override()
 pd.set_option("display.max_rows", None, "display.max_columns", None)
 
 
-def getCrypto(crp, data_start, data_end):
-    crphistory = pdr.get_data_yahoo(crp, start=data_start, end=data_end)
-
+def getCrypto(crp, data_start, data_end, interval):
+    crphistory = pdr.get_data_yahoo(crp, start=data_start, end=data_end, interval=interval)
+    crphistory = crphistory.reset_index()
+    for i in ['Open', 'High', 'Close', 'Low']:
+        crphistory[i] = crphistory[i].astype('float64')
     return crphistory
 
 
-def exchange(crp, data_start, data_end, currency, interval=1):
+def exchange(crp, data_start, data_end, currency, interval="1mo"):
     url = 'https://api.exchangerate-api.com/v4/latest/USD'
     converter = RealTimeCurrencyConverter(url)
-    df = getCrypto(crp, data_start, data_end)
+    df = getCrypto(crp, data_start, data_end, interval)
     keys_to_remove = ["Adj Close", "Volume"]
     for key in keys_to_remove:
         df.pop(key)
-    format = "%Y-%m-%d"
-    start = datetime.datetime.strptime(data_start, format)
-    end = datetime.datetime.strptime(data_end, format)
-    delta = datetime.timedelta(days=1)
-    days = end - start
-
-    start -= delta
-
+    
     lista_calosc = {"Date": [], "Open": [], "High": [], "Low": [], "Close": []}
-    data = start
-    if int(interval) > int(days.days):
-        interval = 1
-    for i in range(1, days.days, int(interval)):
-        lista_calosc["Date"].append(str(data)[:10])
-        lista_calosc["Open"].append(converter.convert(
-            "USD", f'{currency}', df["Open"][i]))
-        lista_calosc["High"].append(converter.convert(
-            "USD", f'{currency}', df["High"][i]))
-        lista_calosc["Low"].append(converter.convert(
-            "USD", f'{currency}', df["Low"][i]))
-        lista_calosc["Close"].append(converter.convert(
-            "USD", f'{currency}', df["Close"][i]))
-        data += delta
-    trend = 0
-    for i in range(1, 7):
-        if lista_calosc["Open"][-i] > lista_calosc["Open"][-i-1]:
-            trend = trend + 1
-        elif lista_calosc["Open"][-i] == lista_calosc["Open"][-i-1]:
-            trend = trend + 0
-        else:
-            trend = trend - 1
-    print(trend)
-    if trend >= 2:
-        lista_calosc["Trend"] = "Up trend"
-    elif trend < 2 and trend > -2:
-        lista_calosc["Trend"] = "Flat trend"
-    else:
-        lista_calosc["Trend"] = "Down trend"
 
+    for i in range(len(df["Open"])): 
+        lista_calosc["Date"].append(str(df["Date"][i])[:10])
+        lista_calosc["Open"].append(converter.convert("USD", f'{currency}', df["Open"][i]))
+        lista_calosc["High"].append(converter.convert("USD", f'{currency}', df["High"][i]))
+        lista_calosc["Low"].append(converter.convert("USD", f'{currency}', df["Low"][i]))
+        lista_calosc["Close"].append(converter.convert("USD", f'{currency}', df["Close"][i]))
 
     return lista_calosc
