bafdd0c050df5db8486f3807765eb0f7d9937584
MatHyp <matijoker29@gmail.com>
2022-02-02 17:53:13 +0100

    added two new tests

--- PoC/test.py
+++ PoC/test.py
@@ -7,12 +7,14 @@ from createFrontData import *
 from api import *
 from chart import *
 
+
 def test1():
     """Test 1: Testing the output of new predict function - Kuba"""
     x = predict()
     if x == "Up trend" or "Flat trend" or "Down trend":
         assert True
-    
+
+
 def test2():
     """Test 2: Testing price getting of crypto - Dawid"""
     y = getCrypto('BTC-USD', tydzien, now, "1d")
@@ -23,24 +25,47 @@ def test2():
     if y == float:
         assert True
 
+
 def test3():
     """Test 3: Testing out, the crypto release definition - Piotrek"""
     x = to_percentage("BTC-USD")
     if (len(str(x)) == 5):
         assert True
 
+
 def test4():
     """Test 4: Testing currency dictionary length - Bartek"""
-    x = addToList(currency= "USD")
+    x = addToList(currency="USD")
     if len(x[0].keys()) == 5:
-       assert True
+        assert True
+
 
 def test5():
     """Test 5: Testing returning the random crypto name - Kuba"""
     x = currentNameFunc('BTC-USD', 'GBP')
     # List of all available cryptos
-    y = ['Bitcoin', 'Ethernum', 'Binance Coin', 'Dogecoin', 'Avalanche', 'Tether', 'Chainlink', 'Solana' ]
+    y = ['Bitcoin', 'Ethernum', 'Binance Coin', 'Dogecoin',
+         'Avalanche', 'Tether', 'Chainlink', 'Solana']
     if x in y:
+        assert True
+    else:
+        assert False
+
+
+def test6():
+    """Test 6: Testing ex funcion output- Mateusz"""
+    x = ex(20, 'PLN')
+
+    if type(x) == float:
+        assert True
+    else:
+        assert False
+
+
+def test7():
+    """Test 7: Testing Cryptocurrencys list length - Mateusz"""
+    x = addToList(currency="USD")
+    if len(x) == 8:
         assert True
     else:
         assert False
